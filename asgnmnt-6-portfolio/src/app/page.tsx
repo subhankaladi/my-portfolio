0e2cf911c12436c7a8c9b35b7d76e93c4ddab43f
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2024-10-18 15:29:42 +0500

    Add files via upload

---
+++ asgnmnt-6-portfolio/src/app/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Home from './component/home/Home'
+import About from './component/about/About'
+import Service from './component/service/Service'
+
+const page = () => {
+  return (
+   <div>
+  
+     <Home />
+     
+    <About />
+   
+    <Service />
+    
+  
+   </div>
+  )
+}
+
+export default page
